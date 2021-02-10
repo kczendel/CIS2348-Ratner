19dfded4da237aa9fe4e53423bac0c2ad9c5f732
kczendel <77933237+kczendel@users.noreply.github.com>
2021-02-10 10:14:07 -0600

    Add files via upload

---
+++ Homework1/CodingProblem_1.py
@@ -0,0 +1,24 @@
+# Kyle Dela Pena
+# 2038252
+
+from datetime import date
+
+
+def calculate_age(current_date, birth_date):
+    today = current_date
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    return age
+
+
+print("Birthday Calculator")
+print('current Day')
+month = int(input('Month: '))
+day = int(input('Day: '))
+year = int(input('Year: '))
+print('Birthday')
+birth_month = int(input('Month: '))
+birth_day = int(input('Day: '))
+birth_year = int(input('Year: '))
+print("you are ", calculate_age(date(year, month, day), date(birth_year, birth_month, birth_day)), "years old.")
+if day == birth_day & month == birth_month & year == birth_year:
+    print("Happy Birthday!")
