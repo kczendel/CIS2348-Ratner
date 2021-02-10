43be6b3b8a1457fdf0acdb2ef37f7a8d5725e7a8
kczendel <77933237+kczendel@users.noreply.github.com>
2021-02-10 13:16:08 -0600

    Update zyLab2.19.py

--- Homework1/zyLab2.19.py
+++ Homework1/zyLab2.19.py
@@ -7,7 +7,8 @@ water_cups = float(input('Enter amount of water (in cups):\n'))
 nectar_cups = float(input('Enter amount of agave nectar (in cups):\n'))
 servings_cups = float(input('How many servings does this make?\n'))
 
-print('\nLemonade ingredients - yields {:.2f} servings'.format(servings_cups))
+print()
+print('Lemonade ingredients - yields {:.2f} servings'.format(servings_cups))
 print('{:.2f} cup(s) lemon juice'.format(lemon_cups))
 print('{:.2f} cup(s) water'.format(water_cups))
 print('{:.2f} cup(s) agave nectar'.format(nectar_cups))
@@ -16,14 +17,16 @@ print('{:.2f} cup(s) agave nectar'.format(nectar_cups))
 
 desired_servings = float(input('\nHow many servings would you like to make?\n'))
 
-print('\nLemonade ingredients - yields {:.2f} servings'.format(desired_servings))
+print()
+print('Lemonade ingredients - yields {:.2f} servings'.format(desired_servings))
 print('{:.2f} cup(s) lemon juice'.format(lemon_cups * desired_servings / servings_cups))
 print('{:.2f} cup(s) water'.format(water_cups * desired_servings / servings_cups))
 print('{:.2f} cup(s) agave nectar'.format(nectar_cups * desired_servings / servings_cups))
 
 # FIXME (3): Convert and output the ingredients from (2) to gallons
 
-print('\nLemonade ingredients - yields {:.2f} servings'.format(desired_servings))
+print()
+print('Lemonade ingredients - yields {:.2f} servings'.format(desired_servings))
 print('{:.2f} gallon(s) lemon juice'.format(lemon_cups * desired_servings / servings_cups / 16))
 print('{:.2f} gallon(s) water'.format(water_cups * desired_servings / servings_cups / 16))
 print('{:.2f} gallon(s) agave nectar'.format(nectar_cups * desired_servings / servings_cups / 16))
