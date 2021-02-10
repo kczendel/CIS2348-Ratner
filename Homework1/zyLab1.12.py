803af3e2cf0efb227109d13f3f48f42bc3fe3253
kczendel <77933237+kczendel@users.noreply.github.com>
2021-02-10 10:08:43 -0600

    Update zyLab1.12.py

--- Homework1/zyLab1.12.py
+++ Homework1/zyLab1.12.py
@@ -1,7 +1,8 @@
-#Kyle Dela Pena
-#2038252
+# Kyle Dela Pena
+# 2038252
 
 userNum = int(input())
 userNumSquared = userNum * userNum  # Bug here; fix it when instructed
 
 print(userNumSquared)  # Output formatting issue here; fix it when instructed
+
