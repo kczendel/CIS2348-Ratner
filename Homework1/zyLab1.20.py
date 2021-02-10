a72265edda726a218872128990d083696c2ae774
kczendel <77933237+kczendel@users.noreply.github.com>
2021-02-10 13:36:26 -0600

    Update zyLab1.20.py

--- Homework1/zyLab1.20.py
+++ Homework1/zyLab1.20.py
@@ -1,12 +1,14 @@
 # Kyle Dela Pena
 # 2038252
 
-user_num = int(input('Enter integer:\n'))
+user_num = int(input('Enter integer:'))
+print()
 
 # Type your code here
 print("You entered:", user_num)
-print(user_num, "squared is", (user_num*user_num))
-print("And", user_num, "cubed is", (user_num*user_num*user_num), "!!")
-user_num2 = int(input('Enter another integer:\n'))
+print(user_num, "squared is", (user_num * user_num))
+print("And", user_num, "cubed is", (user_num * user_num *user_num), "!!")
+user_num2 = int(input('Enter another integer:'))
+print()
 print(user_num, "+", user_num2, "is", (user_num + user_num2))
 print(user_num, "*", user_num2, "is", (user_num * user_num2))
