36657276d1adbefa97838367dc133529776bb3a4
kczendel <77933237+kczendel@users.noreply.github.com>
2021-03-03 12:22:25 -0600

    Update CodingProblem2.py

--- Homework2/CodingProblem2.py
+++ Homework2/CodingProblem2.py
@@ -1,11 +1,17 @@
 # Kyle Dela Pena
 # 2038252
+from datetime import date
 
 month_list = {"JANUARY": "1", "FEBRUARY": "2", "MARCH": "3", "APRIL": "4", "MAY": "5", "JUNE": "6", "JULY": "7",
              "AUGUST": "8", "SEPTEMBER": "9", "OCTOBER": "10", "NOVEMBER": "11", "DECEMBER": "12"}
 
 input_file = open('inputDates.txt', 'r')
 output_file = open('parsedDates.txt', 'w')
+
+today = date.today()
+today1 = today.strftime("%Y")
+today2 = today.strftime("%m")
+today3 = today.strftime("%d")
 
 for line in input_file:
     if line != "-1":
@@ -14,6 +20,8 @@ for line in input_file:
             month = date[0]
             day = date[1]
             year = date[2]
+        if year > today1 or (year > today1 and month > today2) or (year > today1 and month > today2 and day > today3):
+            continue
         if month.upper() in month_list:
             comma = day[-1]
             if comma == ',':
@@ -25,3 +33,7 @@ for line in input_file:
 
 output_file.close()
 input_file.close()
+print(year)
+print(today1)
+print(today2)
+print(today3)
