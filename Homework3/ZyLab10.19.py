5d95787b4df44d0f71020956db8027a4beb3a20c
kczendel <77933237+kczendel@users.noreply.github.com>
2021-03-31 23:49:21 -0500

    Update ZyLab10.19.py

--- Homework3/ZyLab10.19.py
+++ Homework3/ZyLab10.19.py
@@ -5,11 +5,8 @@ class ItemToPurchase:
 
     def __init__(self, name='none', price=0, quantity=0, description='none'):
         self.item_name = name
-
         self.item_description = description
-
         self.item_price = price
-
         self.item_quantity = quantity
 
     def print_item_cost(self):
